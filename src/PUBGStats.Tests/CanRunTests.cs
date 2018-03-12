c6cf5e214483fcdf7b9177e59a547566156c0292
Benn Lazell <benn@redrabbits.co.uk>
2018-03-12 20:03:00 +0000

    NUnit Setup

---
+++ src/PUBGStats.Tests/CanRunTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace PUBGStats.Tests
+{
+    [TestFixture]
+    public class CanRunTests
+    {
+        [Test]
+        public void TestsGonnaTest()
+        {
+            //Given
+            //When
+            var expected = true;
+            
+            //Then
+            Assert.IsTrue(expected);
+        }
+    }
+}
